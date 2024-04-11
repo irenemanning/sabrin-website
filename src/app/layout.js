caa03f154c0b1c603f8f602aada027bac5f2cd18
Irene <irenemanningm@gmail.com>
2024-04-11 01:44:37 -0500

    Add footer to layout.js to appear on page.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Navbar from "./components/navbar/Navbar"
-
+import Footer from "./components/footer/Footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}
       <Navbar />
+      <Footer />
       </body>
     </html>
   )
