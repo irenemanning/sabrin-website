72674c0cf9429248bd6bf05cf9bace078a32d230
Irene <irenemanningm@gmail.com>
2024-07-10 21:04:30 -0500

    Add videos to the Acting page.

--- src/app/acting/page.jsx
+++ src/app/acting/page.jsx
@@ -1,6 +1,24 @@
 function Acting() {
     return (
-      <div>Acting stuff</div>
+      <div>Acting stuff
+        <div>
+          <h1>Bathroom Bromance</h1>
+          <iframe src="https://drive.google.com/file/d/1PhaZTHJgkyzkIdxpcrhb_LJBh6r4WH2p/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+        <div>
+          <h1>Comiendo Mierda</h1>
+          <iframe src="https://drive.google.com/file/d/1NdjPUx1tZaui_zHtsQPGT8Y0K6_gSZ4A/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+        <div>
+          <h1>Calvin Klein Campaigne</h1>
+          <iframe src="https://drive.google.com/file/d/1UZ2eogu00EDEMWNYsWewZsvyW4FMgKNG/preview" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+        <div>
+          <h1>Udder Love</h1>
+          <iframe src="https://www.youtube.com/watch?v=NQGEvvLWryo&t=15s" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+        
+      </div>
     )
   }
   
--- src/app/bio/page.jsx
+++ src/app/bio/page.jsx
@@ -1,6 +1,12 @@
 function Bio() {
     return (
-      <div>Sabrin is a homestead icon who lives in ashville NC</div>
+      <div>
+        <h1>About Sabo</h1>
+        <div>
+          <h1>PBS Artist Statement</h1>
+          <iframe src="https://youtu.be/voXCMvou3iQ?si=q_GXm7p9lSbft7Ea" width="640" height="480" allow="autoplay"></iframe>
+        </div>
+      </div>
     )
   }
   
