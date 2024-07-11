1017b43fba5642b36433671b4b21d6b3f971855b
Irene <irenemanningm@gmail.com>
2024-07-11 10:46:26 -0500

    Replace single quotes with HTML entities and update the JSX to use inline styles or CSS for the border instead of using the deprecated frameBorder attribute.

--- src/app/acting/page.jsx
+++ src/app/acting/page.jsx
@@ -1,42 +1,73 @@
 
 function Acting() {
-    return (
+  const iframeStyle = { borderRadius: '12px', border: 'none' }
+
+  return (
+    <div>
+      HEADSHOTS, Actor Resume, digitals, Prodigal Son monologue,
       <div>
-        HEADSHOTS, Actor Resume, digitals, Prodigal Son monologue,
-        <div>
-          <h1>Somewhere Over The Pacific</h1>
-          <iframe 
-            style={{ borderRadius: '12px', border: 'none' }} 
-            src="https://open.spotify.com/embed/episode/4xP6oaZuyplMz0HvE3YmXu?utm_source=generator&theme=0" 
-            width="100%" 
-            height="352" 
-            allowFullScreen 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy"
-          ></iframe>
-        </div>
-        <div>
-          <h1>Tomorrow's Wish by Wade Bradford</h1>
-          <iframe width="640" height="480" src="https://www.youtube.com/embed/Etdg9fPD0OU?si=niIAwA0OJZ5zTaNA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div>
-          <h1>Prodigal Son monologue</h1>
-        </div>
-        <div>
-          <h1>Conjunction Not A Function</h1>
-          <iframe width="640" height="480" src="https://www.youtube.com/embed/NgnFMl4Laeg?si=79PNffSwjQDPo8Gu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>
-        <div>
-          <h1>Calvin Klein Campaigne</h1>
-          <iframe src="https://drive.google.com/file/d/1UZ2eogu00EDEMWNYsWewZsvyW4FMgKNG/preview" width="640" height="480" allow="autoplay"></iframe>
-        </div>
-        <div>
-          <h1>MUTT</h1>
-          <iframe src="https://drive.google.com/file/d/16aPYzQjEC1L6ELSytcnOKjTKOT2a0ODT/preview" width="640" height="480" allow="autoplay"></iframe>
-        </div>
-        
+        <h1>Somewhere Over The Pacific</h1>
+        <iframe
+          style={iframeStyle}
+          src="https://open.spotify.com/embed/episode/4xP6oaZuyplMz0HvE3YmXu?utm_source=generator&theme=0"
+          width="100%"
+          height="352"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
       </div>
-    )
-  }
-  
-  export default Acting
+      <div>
+        <h1>Tomorrow&apos;s Wish by Wade Bradford</h1>
+        <iframe
+          width="640"
+          height="480"
+          src="https://www.youtube.com/embed/Etdg9fPD0OU?si=niIAwA0OJZ5zTaNA"
+          title="YouTube video player"
+          style={{ border: 'none' }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div>
+        <h1>Prodigal Son monologue</h1>
+      </div>
+      <div>
+        <h1>Conjunction Not A Function</h1>
+        <iframe
+          width="640"
+          height="480"
+          src="https://www.youtube.com/embed/NgnFMl4Laeg?si=79PNffSwjQDPo8Gu"
+          title="YouTube video player"
+          style={{ border: 'none' }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div>
+        <h1>Calvin Klein Campaigne</h1>
+        <iframe
+          src="https://drive.google.com/file/d/1UZ2eogu00EDEMWNYsWewZsvyW4FMgKNG/preview"
+          width="640"
+          height="480"
+          allow="autoplay"
+          style={{ border: 'none' }}
+        ></iframe>
+      </div>
+      <div>
+        <h1>MUTT</h1>
+        <iframe
+          src="https://drive.google.com/file/d/16aPYzQjEC1L6ELSytcnOKjTKOT2a0ODT/preview"
+          width="640"
+          height="480"
+          allow="autoplay"
+          style={{ border: 'none' }}
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default Acting
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,7 @@ function Contact() {
   return (
     <div>
       image of sabo
-      <h1>Let's Get in Touch!</h1>
+      <h1>Let&apos;s Get in Touch!</h1>
       <h2>Send Me an Email:</h2>
       <h3>sabridiehl1@gmail.com</h3>
     </div>
