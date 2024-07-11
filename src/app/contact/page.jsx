9f04a2552c4ce731094fea62c02c0118c27f7330
Irene <irenemanningm@gmail.com>
2024-07-11 10:22:27 -0500

    Add Spotify enbed to acting page, organize video iframes to different tabs.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,7 +2,7 @@
 function Contact() {
   return (
     <div>
-      <img src="" alt="Pic Of Sabo" />
+      image of sabo
       <h1>Let's Get in Touch!</h1>
       <h2>Send Me an Email:</h2>
       <h3>sabridiehl1@gmail.com</h3>
