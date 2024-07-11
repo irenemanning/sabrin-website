a39820c1f0b9a9f419f0e041066334f99a8e3be5
Irene <irenemanningm@gmail.com>
2024-07-11 10:52:12 -0500

    Adjust video width on Films page to make all iframes uniform.

--- src/app/films/page.jsx
+++ src/app/films/page.jsx
@@ -18,7 +18,7 @@ function Films() {
         </div>
         <div>
             <h1>Zoom: Love on the Fast-Track</h1>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/F75ZKbuorBM?si=epSN0jG-aVlnD474" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="640" height="480" src="https://www.youtube.com/embed/F75ZKbuorBM?si=epSN0jG-aVlnD474" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div>
           <h1>A Day In The Moo Life</h1>
