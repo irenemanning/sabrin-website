c8ad057813ec4e7ac8f7981eba44066b340ca1bf
Irene <irenemanningm@gmail.com>
2024-07-15 12:13:00 -0500

    Add more video embeds.

--- src/app/acting/page.jsx
+++ src/app/acting/page.jsx
@@ -31,6 +31,10 @@ function Acting() {
         ></iframe>
       </div>
       <div>
+        <h1>Prodigal Son by John Patrick Shanley</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/c5qgeRSOauA?si=u6Yib_HcOSioo4p0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div>
         <h1>Prodigal Son monologue</h1>
       </div>
       <div>
--- src/app/comedy/page.jsx
+++ src/app/comedy/page.jsx
@@ -1,7 +1,14 @@
 function Comedy() {
     return (
       <div>
-        <h1>stand up sets, Ernie video?, improv?</h1>
+        <div>
+          <h1>Ernie has therapy on Zoom for the first time during the 2020 lockdown</h1>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/4vGl0DTBZIY?si=wAgx6D3XzA4fvGdD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div>
+          <h1>Stand-up set, MSP, 06/10/24</h1>
+        </div>
+        
       </div>
     )
   }
