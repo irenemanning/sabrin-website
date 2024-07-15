001d4839108a5f559d3317263ec263db1992493f
Irene <irenemanningm@gmail.com>
2024-07-15 17:11:15 -0500

    Add images and styling to About page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,8 +2,17 @@ function About() {
     return (
       <div>
         <h1>About Sabo</h1>
-        <h3>LONG BIO</h3>
-        <div>
+        <div className="flex flex-col md:flex-row md:items-start">
+          <div className="flex flex-col md:w-1/3 space-y-4">
+            <img src="/photos/Sabrin-123.jpg" alt="" className="md:w-auto rounded shadow-lg" />
+            <img src="/photos/Sabrin-5.jpg" alt="" className="md:w-auto rounded shadow-lg" />
+            <img src="/photos/Sabrin-225.jpg" alt="" className="md:w-auto rounded shadow-lg" />
+          </div>
+          <h3 className="flex-1 md:ml-20">
+            LONG BIO LONG BIO LONG BIO LONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIO LONG BIO LONG BIO LONG BIO LONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG BIOLONG
+          </h3>
+        </div>
+        <div className="my-20">
           <h1>PBS Artist Statement</h1>
           <iframe
             width="560"
