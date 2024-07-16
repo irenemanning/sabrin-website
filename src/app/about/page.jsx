0d3bf52bc4d6aa1bdfa3f168c99c3fc0547461de
Irene <irenemanningm@gmail.com>
2024-07-15 20:26:52 -0500

    Fix syntax issues in contact and acting pages.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,41 +1,41 @@
 function About() {
-    return (
-      <div>
-        <div className="flex flex-col md:flex-row md:items-start">
-          <div className="flex flex-col md:w-1/3 space-y-4">
-            <img src="/photos/Sabrin-123.jpg" alt="" className="md:w-auto rounded shadow-lg" />
-            <img src="/photos/Sabrin-5.jpg" alt="" className="md:w-auto rounded shadow-lg" />
-            {/* <img src="/photos/Sabrin-225.jpg" alt="" className="md:w-auto rounded shadow-lg" /> */}
-          </div>
-          
-          <div className="flex-1 md:ml-20">
-            <h3 className="md:ml-10 mt-5 mb-5">
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row md:items-start">
+        <div className="flex flex-col md:w-1/3 space-y-4">
+          <img src="/photos/Sabrin-123.jpg" alt="" className="md:w-auto rounded shadow-lg" />
+          <img src="/photos/Sabrin-5.jpg" alt="" className="md:w-auto rounded shadow-lg" />
+          {/* <img src="/photos/Sabrin-225.jpg" alt="" className="md:w-auto rounded shadow-lg" /> */}
+        </div>
+        
+        <div className="flex-1 md:ml-20">
+          <h3 className="md:ml-10 mt-5 mb-5">
             Sabrín Diehl (they/he) is a Miami based interdisciplinary artist; Working within the mediums of performance art, comedy, writing, acting, and filmmaking. They are most known for having created, written, produced and performed in largely well-received original sketch-comedy plays and one person shows. However, they also enjoy the quieter forms such as photography, poetry, and drawing. 
-            </h3>
+          </h3>
 
-            <h3 className="md:ml-10 mt-5 mb-5">
-            Sabrín’s most recent work was acting in the world premiere of #Graced at the Adrienne Arsht Center. They have created an original sketch-comedy play, Latins on ICE, that premiered on the frozen Lake Nokomis in Minneapolis, MN. He has a one-person radio play Somewhere Over the Rainbow, produced by Picnic Basket Theater, available on all podcast streaming platforms. They have also created, written, directed, and produced multiple independent short-films. They have worked with the Guthrie Theater, Mixed Blood Theater, Abre Camino Collective, Penumbra Theatre, Black Label Movement, HUGE Theater, and many more. They have trained with The Margolis Method and the Guthrie Theater and are a graduate of New World School of the Arts (Miami) and of the University of Minnesota (Twin Cities) with a B.A. in Theater. 
-            </h3>
+          <h3 className="md:ml-10 mt-5 mb-5">
+            Sabrín&apos;s most recent work was acting in the world premiere of #Graced at the Adrienne Arsht Center. They have created an original sketch-comedy play, Latins on ICE, that premiered on the frozen Lake Nokomis in Minneapolis, MN. He has a one-person radio play Somewhere Over the Rainbow, produced by Picnic Basket Theater, available on all podcast streaming platforms. They have also created, written, directed, and produced multiple independent short-films. They have worked with the Guthrie Theater, Mixed Blood Theater, Abre Camino Collective, Penumbra Theatre, Black Label Movement, HUGE Theater, and many more. They have trained with The Margolis Method and the Guthrie Theater and are a graduate of New World School of the Arts (Miami) and of the University of Minnesota (Twin Cities) with a B.A. in Theater. 
+          </h3>
 
-            <h3 className="md:ml-10 mt-5 mb-5">Sabrín hosts alternative comedy shows at his house and has near future plans of beginning an alternative comedy show in their Miami hometown.</h3>
-          </div>
-          
+          <h3 className="md:ml-10 mt-5 mb-5">Sabrín hosts alternative comedy shows at his house and has near future plans of beginning an alternative comedy show in their Miami hometown.</h3>
         </div>
-        <div className="my-20">
-          <h1>Short Documentary On Sabrín’s Work</h1>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/voXCMvou3iQ?si=VP6K81vNw4f_keR6"
-            title="YouTube video player"
-            style={{ border: 'none' }}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
+        
       </div>
-    )
-  }
-  
-  export default About
+      <div className="my-20">
+        <h1>Short Documentary On Sabrín&apos;s Work</h1>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/voXCMvou3iQ?si=VP6K81vNw4f_keR6"
+          title="YouTube video player"
+          style={{ border: 'none' }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,12 +4,11 @@ function Contact() {
     <div className="flex items-center justify-center space-x-4">
       <img src="/photos/FUNphoto1.jpg" alt="" className="md:w-1/3 rounded shadow-lg" />
       <div>
-        <h1>Let's Get in Touch!</h1>
-        <h2>Send Me an Email:</h2>
+        <h1>Let&apos;s Get in Touch!</h1>
+        <h2>Email:</h2>
         <h3>sabridiehl1@gmail.com</h3>
-         <h3>Follow me on Instagram @soysabrin</h3>
+        <h3>Follow me on Instagram @soysabrin</h3>
       </div>
-    
     </div>
   )
 }
